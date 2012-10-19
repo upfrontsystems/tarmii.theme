84d0f6321200eda2f7d82ff16189bf8015e9dd81
Roché Compaan <roche@upfrontsystems.co.za>
2012-10-19 14:19:03 +0200

    more generic selector for add buttons
    add overlay for a.edit_link

--- tarmii/theme/static/js/overlays.js
+++ tarmii/theme/static/js/overlays.js
@@ -1,7 +1,16 @@
 $(function($) {
     var url = location.href;
 
-    $('#add-resource-form input').prepOverlay({
+    $('#add-button-form input').prepOverlay({
+        subtype: 'ajax',
+        filter: '#content>*',
+        formselector: '#form',
+        noform: 'reload',
+        redirect: url,
+        closeselector: '[name=form.buttons.cancel]'
+        });
+
+    $('a.edit_link').prepOverlay({
         subtype: 'ajax',
         filter: '#content>*',
         formselector: '#form',
