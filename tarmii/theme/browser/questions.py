2933876685450ad19b9007dfc47dbe6a4715bb97
Roché Compaan <roche@upfrontsystems.co.za>
2012-10-30 13:37:57 +0200

    create content and redirect to its edit form

--- tarmii/theme/browser/questions.py
+++ tarmii/theme/browser/questions.py
@@ -1,5 +1,6 @@
-from five import grok
 from zope.interface import Interface
+from five import grok
+from plone.dexterity.utils import createContentInContainer
 from collective.topictree.topictree import ITopicTree
 
 grok.templatedir('templates')
@@ -26,3 +27,9 @@ class AddQuestionView(grok.View):
     grok.template('addquestion')
     grok.require('cmf.AddPortalContent')
 
+    def update(self):
+        if not self.request.has_key('form.submitted'):
+            return
+        obj = createContentInContainer(self.context,
+            'upfront.assessmentitem.content.assessmentitemcontainer')
+        self.request.RESPONSE.redirect(obj.absolute_url() + '/edit')
