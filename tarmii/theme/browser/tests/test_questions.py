609ab1c47380ac4874436d790b3c853ce8db2bef
Roché Compaan <roche@upfrontsystems.co.za>
2012-10-30 12:33:45 +0200

    remove test, method was removed

--- tarmii/theme/browser/tests/test_questions.py
+++ tarmii/theme/browser/tests/test_questions.py
@@ -8,8 +8,3 @@ class TestQuestions(TarmiiThemeTestBase):
         view = self.questions.restrictedTraverse('@@questions')
         self.assertEqual(len(view.questions()), 0)
 
-    def test_add_question_url(self):
-        view = self.questions.restrictedTraverse('@@questions')
-        self.assertEqual(view.add_question_url(), 
-            '%s/++add++upfront.assessmentitem.content.assessmentitemcontainer' %
-            self.questions.absolute_url())
