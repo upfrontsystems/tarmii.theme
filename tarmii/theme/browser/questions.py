c5c7fb66b9f47fecb4cae912eb4def62af620caa
Roché Compaan <roche@upfrontsystems.co.za>
2012-11-05 10:15:45 +0200

    remove AddQuestionsView

--- tarmii/theme/browser/questions.py
+++ tarmii/theme/browser/questions.py
@@ -18,20 +18,3 @@ class QuestionsView(grok.View):
         """
         return self.context.getFolderContents()
 
-
-class AddQuestionView(grok.View):
-    """ Add Questions View
-    """
-    grok.context(Interface)
-    grok.name('addquestion')
-    grok.template('addquestion')
-    grok.require('cmf.AddPortalContent')
-
-    def update(self):
-        if not self.request.has_key('form.submitted'):
-            return
-        obj = createContentInContainer(self.context,
-            'upfront.assessmentitem.content.assessmentitemcontainer')
-        createContentInContainer(obj,
-            'upfront.assessmentitem.content.assessmentitem')
-        self.request.RESPONSE.redirect(obj.absolute_url() + '/edit')
