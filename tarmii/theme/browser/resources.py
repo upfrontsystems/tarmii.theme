7c6f958b1e2759957bc2ec90abacfe33c3d01b7e
witek <witekdev@gmail.com>
2012-11-08 18:05:29 +0200

    add check for AddPortalContent permission on resources folder.

--- tarmii/theme/browser/resources.py
+++ tarmii/theme/browser/resources.py
@@ -1,8 +1,11 @@
 import json
 from five import grok
 
+from AccessControl import getSecurityManager
+
 from zope.interface import Interface
 from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.permissions import AddPortalContent
 
 from tarmii.theme import MessageFactory as _
 
@@ -20,6 +23,13 @@ class ResourcesView(grok.View):
         catalog = getToolByName(self.context, 'portal_catalog')
         brains = catalog(portal_type='collective.topictree.topictree')
         return brains
+
+    def addresource_visible(self):
+        """ test if add resource button can be shown based on whether the user
+            has "AddPortalContent" Permission on the resouces folder
+        """
+        return getSecurityManager().checkPermission(AddPortalContent,
+                                                    self.context)
 
     def add_resource_button_path(self):
         """ Path string for the Add Resource button
