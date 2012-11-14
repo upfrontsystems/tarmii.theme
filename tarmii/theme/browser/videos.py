104684ed66630d509a4c9185be01619323c498e5
witek <witekdev@gmail.com>
2012-11-14 10:01:12 +0200

    add video button to videos view, some code clean up.

--- tarmii/theme/browser/videos.py
+++ tarmii/theme/browser/videos.py
@@ -1,11 +1,11 @@
 from five import grok
-from subprocess import call
 
+from AccessControl import getSecurityManager
 from Acquisition import aq_inner
-#from Products.CMFCore.utils import getToolByName
 from zope.interface import Interface
 
 from Products.CMFPlone.PloneBatch import Batch
+from Products.CMFCore.permissions import AddPortalContent
 
 from tarmii.theme import MessageFactory as _
 
@@ -25,7 +25,27 @@ class VideosView(grok.View):
         brains = self.context.getFolderContents()
         b_size = 5
         b_start = self.request.get('b_start', 0)
-        import pdb; pdb.set_trace()
         return Batch(brains, b_size, int(b_start), orphan=0)
+
+    # this might not be necessary if we upgrade to collective.quickupload
+    # as it includes a permissions check already.
+    def addvideo_visible(self):
+        """ test if add video button can be shown based on whether the user
+            has "AddPortalContent" Permission on the resouces folder
+        """
+        return getSecurityManager().checkPermission(AddPortalContent,
+                                                    self.context)
+
+    def add_video_button_path(self):
+        """ Path string for the Add Resource button
+        """
+        # XXX: we need some sort of check to make sure only flv extensions are
+        # allowed to be uploaded.
+        # The title of the file (video ) must also be non-optional
+        return '%s/createObject?type_name=File' % self.context.absolute_url()
+
+
+
+
 
 
