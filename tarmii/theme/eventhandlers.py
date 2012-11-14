093794d558fb4b65179997d3edbfd83e72d2d6e8
witek <witekdev@gmail.com>
2012-11-14 16:53:14 +0200

    onVideoAdded event now creates a thumbnail image in the videos folder after a video is added.

--- tarmii/theme/eventhandlers.py
+++ tarmii/theme/eventhandlers.py
@@ -1,5 +1,8 @@
+import os
+import tempfile
 from five import grok
 from Acquisition import aq_parent
+from subprocess import call
 
 from Products.ATContentTypes.interfaces.file import IFileContent
 from Products.Archetypes.interfaces import IObjectInitializedEvent
@@ -9,10 +12,24 @@ def onVideoAdded(video, event):
     """ Create Thumbnail file from a video file.
     """
 
-    # test that the object created was added to the videos folder (is a video)
+    # test that the object created was added to the videos folder 
+    # (test that this File is indeed a video file.)
     if video.aq_parent.Title() != 'Videos':
         return
     
-    print 'Video added'
+    tmp = tempfile.NamedTemporaryFile()
+    tmp.write(video.data)
 
+    thumb = tempfile.gettempdir() + '/tmp.jpg'
+    # take the 5th second of video for thumb, -y overwrites old tmp.jpg files.
+    call(["ffmpeg", "-itsoffset", "-5", "-i", tmp.name, "-vcodec", "mjpeg", "-y",
+          "-vframes", "1", "-an", "-f", "rawvideo", "-s", "320x180", thumb])
 
+    #read in tmp.jpg 
+    cwd = os.path.join(tempfile.gettempdir(), 'tmp.jpg')
+    f = open(cwd, "rb")
+    fileRawData = f.read()
+
+    # create an image object from tmp.jpg data
+    video_id = video.title + '_'
+    video.aq_parent.invokeFactory('Image', video_id, image=fileRawData)
