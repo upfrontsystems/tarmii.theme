aa5739e1f23e40284cc7427e2a3543f81f83cccf
witek <witekdev@gmail.com>
2012-11-17 20:36:05 +0200

    added AssessmentItemFeedbackForm and corresponding template for feedback on assessment items, create more complete email message for site contact us form.

--- tarmii/theme/browser/feedback.py
+++ tarmii/theme/browser/feedback.py
@@ -25,6 +25,16 @@ class FeedbackForm(PageForm):
     """ Contact us feedback form 
     """
 
+    MESSAGE = u"""
+    Message from user: %s
+
+    Subject: %s
+    Message:
+
+    %s
+
+    """
+
     label = u'Contact Us'
     form_fields = form.Fields(IFeedbackForm)
     result_template = ViewPageTemplateFile('templates/feedback_result.pt')
@@ -42,7 +52,9 @@ class FeedbackForm(PageForm):
         self.mFrom = user_name + ' <' + user_email + '>'
         self.mTo = email_from
         self.mSubject = data['subject']
-        self.mBody = data['message']
+        self.mBody = self.MESSAGE % (self.mFrom,
+                                     data['subject'],
+                                     data['message'])
 
         try:
             mhost.send(self.mBody, self.mTo, self.mFrom, self.mSubject, 
@@ -52,3 +64,66 @@ class FeedbackForm(PageForm):
             raise SMTPRecipientsRefused('Recipient address rejected by server')
 
         return self.result_template()
+
+
+class IAssessmentItemFeedbackForm(Interface):
+    """ Assessment Item Feedback form schema
+    """
+
+    subject = TextLine(title=u'Subject',
+                       description=u'Subject',
+                       required=True)
+
+    feedback = Text(title=u'Feedback',
+                   description=u'The message body',
+                   required=True)
+
+
+class AssessmentItemFeedbackForm(PageForm):
+    """ Assessment Item feedback form 
+    """
+
+    MESSAGE = u"""
+    Message from user: %s
+    regarding item: %s
+
+    Subject: %s
+    Message:
+
+    %s
+
+    """
+
+    label = u'Feedback'
+    form_fields = form.Fields(IAssessmentItemFeedbackForm)
+    result_template = ViewPageTemplateFile(
+        'templates/assessmentitem_feedback_result.pt')
+
+    @form.action("send")
+    def action_send(self, action, data):
+
+        mhost = getToolByName(self.context, 'MailHost')
+        pm = getToolByName(self.context, 'portal_membership')
+
+        user_name = pm.getAuthenticatedMember().getUserName()
+        user_email = pm.getAuthenticatedMember().email
+        email_from = pm.email_from_address
+        self.assessment_item_id = self.context.id
+
+        self.mFrom = user_name + ' <' + user_email + '>'
+        self.mTo = email_from
+        self.mSubject = data['subject']
+        self.mBody = self.MESSAGE % (self.mFrom,
+                                     self.assessment_item_id,
+                                     data['subject'],
+                                     data['feedback'])
+
+        try:
+            mhost.send(self.mBody, self.mTo, self.mFrom, self.mSubject, 
+                       immediate=True)
+        except SMTPRecipientsRefused:
+            # Don't disclose email address on failure
+            raise SMTPRecipientsRefused('Recipient address rejected by server')
+
+        return self.result_template()
+
