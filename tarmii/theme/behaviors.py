381b16157eec4af3a85780a698f6863545e5d9b3
witek <witekdev@gmail.com>
2013-01-22 19:24:22 +0200

    add IRating behavior, update dependency on datagridfield.

--- tarmii/theme/behaviors.py
+++ tarmii/theme/behaviors.py
@@ -1,8 +1,10 @@
-from zope.interface import alsoProvides
-
+from zope.interface import Interface, alsoProvides
+from zope import schema
 from plone.directives import form
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.namedfile.field import NamedBlobFile
+
+from collective.z3cform.datagridfield import DataGridFieldFactory, DictRow
 
 from tarmii.theme import MessageFactory as _
 
@@ -26,4 +28,28 @@ class IAssessmentItemBlobs(form.Schema):
             required=False,
         )
 
+class IRatingFieldSchema(Interface):
+    """ Schema for rating scale datagrid field, stores the label and rating
+        value.
+    """
+
+    label = schema.TextLine(title=u"Label")
+    rating = schema.Int(title=u"Rating")
+
+class IRating(form.Schema):
+    """ Behavior that enables each assessmentitem to set its rating scale/range.
+    """
+
+    form.fieldset('rating',
+            label=u"Rating",
+            fields=['rating_scale']
+        )
+
+    form.widget(rating_scale=DataGridFieldFactory)
+    rating_scale = schema.List(
+            title=u"Rating scale",
+            value_type=DictRow(title=u"tablerow", schema=IRatingFieldSchema)
+        )
+
 alsoProvides(IAssessmentItemBlobs, IFormFieldProvider)
+alsoProvides(IRating, IFormFieldProvider)
