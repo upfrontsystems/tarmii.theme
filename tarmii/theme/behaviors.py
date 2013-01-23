e20f86e24e36c54773a0da0c3a35e559c4951bb4
witek <witekdev@gmail.com>
2013-01-23 21:38:22 +0200

    add RatingValidator for IRating behavior.

--- tarmii/theme/behaviors.py
+++ tarmii/theme/behaviors.py
@@ -1,5 +1,8 @@
-from zope.interface import Interface, alsoProvides
+from sets import Set
+from five import grok
+from zope.interface import Interface, alsoProvides, Invalid
 from zope import schema
+from z3c.form import validator
 from plone.directives import form
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.namedfile.field import NamedBlobFile
@@ -56,6 +59,29 @@ class IRating(form.Schema):
         )
 
 
-
 alsoProvides(IAssessmentItemBlobs, IFormFieldProvider)
 alsoProvides(IRating, IFormFieldProvider)
+
+
+class RatingValidator(validator.SimpleFieldValidator):
+    
+    def validate(self, value):
+        super(RatingValidator, self).validate(value)
+
+        ratings_set = Set([])
+        for x in range(len(value)):
+            rating = value[x]['rating']            
+            if rating <= 0:
+                raise Invalid(_(u"All rating values must be positive and\
+                                 larger than zero."))
+            if rating in ratings_set:
+                raise Invalid(_(u"All rating values must be unique."))
+            else:
+                ratings_set.add(rating)
+      
+
+validator.WidgetValidatorDiscriminators(RatingValidator,
+                                        field=IRating['rating_scale'])
+grok.global_adapter(RatingValidator)
+
+
