46b5d6889101321f788cd60ae161a32ad484fb4a
witek <witekdev@gmail.com>
2013-02-24 16:08:33 +0200

    recreationg of resources view, filtering system not wired up yet.

--- tarmii/theme/browser/resources.py
+++ tarmii/theme/browser/resources.py
@@ -1,28 +1,22 @@
-import json
 from five import grok
 
 from AccessControl import getSecurityManager
-
 from zope.interface import Interface
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.permissions import AddPortalContent
+from Products.CMFPlone.PloneBatch import Batch
 
 from tarmii.theme import MessageFactory as _
 
 grok.templatedir('templates')
 
 class ResourcesView(grok.View):
-    """ A view to display the all the resources (as topic trees) in the system.
+    """ A view to display the all the teacher resources in the system.
     """
     grok.context(Interface)
     grok.name('resources')
     grok.template('resources')
     grok.require('zope2.View')
-
-    def topictrees(self):
-        catalog = getToolByName(self.context, 'portal_catalog')
-        brains = catalog(portal_type='collective.topictree.topictree')
-        return brains
 
     def addresource_visible(self):
         """ test if add resource button can be shown based on whether the user
@@ -36,115 +30,27 @@ class ResourcesView(grok.View):
         """
         return '%s/createObject?type_name=File' % self.context.absolute_url()
 
-class TopicResourcesView(grok.View):
-    """ A view to display, in detail, the resources associated with a topic.
-    """
-    grok.context(Interface)
-    grok.name('topicresources')
-    grok.template('topicresources')
-    grok.require('zope2.View')
-
-    def topictitle(self):
-        """ Return the title of a specific topic.
-        """
-        topic_uid = self.request.get('topic_uid', '')
+    def topictrees(self):
         catalog = getToolByName(self.context, 'portal_catalog')
-        brains = catalog(UID=topic_uid)
-        return brains[0].Title
-
-    def itemcount(self):
-        """ Return number of resource items that are referenced by topic
-        """
-        topic_uid = self.request.get('topic_uid', '')
-        rc = getToolByName(self.context, 'reference_catalog')
-        brains = rc(targetUID=topic_uid, relationship='topics')
-        return len(brains)
+        brains = catalog(portal_type='collective.topictree.topictree')
+        return brains
 
     def resources(self):
-        """ Return resource files associated with a topic
+        """ Return resource items that match current filter criteria.
+            Batch the result.
         """
-        topic_uid = self.request.get('topic_uid', '')
-        rc = getToolByName(self.context, 'reference_catalog')
-        brains = rc(targetUID=topic_uid, relationship='topics')
-        
-        resources = []
-        pc = getToolByName(self.context, 'portal_catalog')
-        for b in brains:
-            resource_uid = b.getObject().sourceUID      
-            resource = pc(UID=resource_uid)[0].getObject()
-            resources.append(resource)
-
-        return resources
-
-class TreeDataView(grok.View):
-    """ Return the JSON representation of the entire Topic Tree
-    """
-    grok.context(Interface)
-    grok.name('treedata') 
-    grok.require('zope2.View')
-
-    def __call__(self):
-        """ get the JSON representation of the topic tree
-        """
-        # get the root object uid from the request
-        context_node_uid = self.request.get('context_node_uid', '')
-
-        # get the JSON representation of the topic tree
-        # call topicjson on tree root
-        return json.dumps(self.topicjson(context_node_uid))
-
-    def render(self):
-        """ No-op to keep grok.View happy
-        """
-        return ''
-
-    def itemcount(self, node_uid):
-        """ Return number of resource items that are referenced by topic
-        """
-        rc = getToolByName(self.context, 'reference_catalog')
-        brains = rc(targetUID=node_uid, relationship='topics')
-        return len(brains)
-
-    def topicjson(self, node_uid):
         catalog = getToolByName(self.context, 'portal_catalog')
-        brains = catalog(UID=node_uid)
-        contents = brains[0].getObject().getFolderContents()
-        node_name = brains[0].Title
+        folder_path = '/'.join(self.context.getPhysicalPath())
+        results = catalog(path={'query': folder_path, 'depth': 1})
+        b_size = 10
+        b_start = self.request.get('b_start', 0)
+        return Batch(results, b_size, int(b_start), orphan=0)
 
-        # node_rel should be default unless it is a root node,
-        # root nodes are open by default and are non-clickable - invalid url (#)
-        if brains[0].portal_type == 'collective.topictree.topictree':
-            node_rel = 'root'
-            node_state = 'open'
-            url = '#'
-        else:
-            node_rel = 'default'
-            node_name = node_name + ' (' + str(self.itemcount(node_uid)) + ')'
-            node_state = 'closed'
-            url = '%s/@@topicresources?topic_uid=%s' % (
-                  self.context.aq_parent.absolute_url(),
-                  node_uid
-            )
+    def resource_count(self):
+        """ Return number of resource items that match current filter criteria
+        """
+        catalog = getToolByName(self.context, 'portal_catalog')
+        folder_path = '/'.join(self.context.getPhysicalPath())
+        results = catalog(path={'query': folder_path, 'depth': 1})
+        return len(results)
 
-        if len(contents) == 0: 
-            # Tree leaf (no state information)
-            data = {
-                'data': node_name,
-                'attr': {'node_uid': node_uid, 'id': node_uid, 'url' : url, },
-                'rel': node_rel,
-                'children': [],
-            }
-        else:
-            # Non Tree leaf
-            data = {
-                'data': node_name,
-                'attr': {'node_uid': node_uid, 'id': node_uid, 'url' : url, },
-                'rel': node_rel,
-                'state' : node_state,
-                'children': [],
-            }
-
-        for brain in contents:
-            data['children'].append(self.topicjson(brain.UID))
-
-        return data
