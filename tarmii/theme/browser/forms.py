c9dba5ab70a88336d8decd281e50c598285ea29e
witek <witekdev@gmail.com>
2013-02-28 17:50:12 +0200

    simplification of teacher resource add form.

--- tarmii/theme/browser/forms.py
+++ tarmii/theme/browser/forms.py
@@ -11,18 +11,7 @@ class TeacherResourceAddForm(dexterity.AddForm):
     grok.name('tarmii.theme.content.teacherresource')
     grok.layer(ITARMIIThemeLayer)
 
-    # this method needed so we can successfully point our nextURL method
-    def createAndAdd(self, data):
-        teacherresource = super(TeacherResourceAddForm, self).createAndAdd(data)
-        # Acquisition wrap patient in the current context
-        self.teacherresource = teacherresource.__of__(self.context)
-        return self.teacherresource
-
     def nextURL(self):
-        if self.request.form.has_key('form.buttons.cancel'):
-            site = getSite()
-            return '%s/resources' % (site.absolute_url())
-        else:
-            # overwrite to point to the prescription view
-            teacherresource = self.teacherresource
-            return teacherresource.aq_parent.absolute_url()
+        # both save and cancel buttons redirect to the same place
+        site = getSite()
+        return '%s/resources' % (site.absolute_url())
