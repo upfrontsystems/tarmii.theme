2bb6c9dc53fb74efe99fbb70d465bc8f6a42ae7e
witek <witekdev@gmail.com>
2013-02-28 16:29:45 +0200

    custom add form for teacher resource content type, so that we can redirect to teacher resources view after creation.

---
+++ tarmii/theme/browser/forms.py
@@ -0,0 +1,28 @@
+from five import grok
+from plone.directives import dexterity
+from tarmii.theme import MessageFactory as _
+from tarmii.theme.interfaces import ITARMIIThemeLayer
+
+grok.templatedir('templates')
+grok.layer(ITARMIIThemeLayer)
+
+class TeacherResourceAddForm(dexterity.AddForm):
+    grok.name('tarmii.theme.content.teacherresource')
+    grok.layer(ITARMIIThemeLayer)
+
+    # this method needed so we can successfully point our nextURL method
+    def createAndAdd(self, data):
+        teacherresource = super(TeacherResourceAddForm, self).createAndAdd(data)
+
+        # Acquisition wrap patient in the current context
+        self.teacherresource = teacherresource.__of__(self.context)
+
+        return self.teacherresource
+
+    def nextURL(self):
+        if self.request.form.has_key('form.buttons.cancel'):
+            return self.handleCancel()
+        else:
+            # overwrite to point to the prescription view
+            teacherresource = self.teacherresource
+            return teacherresource.aq_parent.absolute_url()
