0870af543a7399fcf201a92ae082a9519cb71e4c
witek <witekdev@gmail.com>
2013-03-05 18:55:44 +0200

    add custom edit form for teacher resources so that we can redirect back to resources view after save/cancel actions.

--- tarmii/theme/browser/forms.py
+++ tarmii/theme/browser/forms.py
@@ -3,6 +3,7 @@ from zope.component.hooks import getSite
 from plone.directives import dexterity
 from tarmii.theme import MessageFactory as _
 from tarmii.theme.interfaces import ITARMIIThemeLayer
+from tarmii.theme.content.teacherresource import ITeacherResource
 
 grok.templatedir('templates')
 grok.layer(ITARMIIThemeLayer)
@@ -10,6 +11,15 @@ grok.layer(ITARMIIThemeLayer)
 class TeacherResourceAddForm(dexterity.AddForm):
     grok.name('tarmii.theme.content.teacherresource')
     grok.layer(ITARMIIThemeLayer)
+
+    def nextURL(self):
+        # both save and cancel buttons redirect to the same place
+        site = getSite()
+        return '%s/resources' % (site.absolute_url())
+
+class TeacherResourceAddForm(dexterity.EditForm):
+    grok.name('edit')
+    grok.context(ITeacherResource)
 
     def nextURL(self):
         # both save and cancel buttons redirect to the same place
