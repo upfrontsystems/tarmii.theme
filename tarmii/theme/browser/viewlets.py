de157420189f47bf0475d74404693b6e7b4c6011
witek <witekdev@gmail.com>
2013-03-06 15:05:38 +0200

    remove template from viewlet, strip action_ prefix from i18ntranslate labels in actions.xml, update translations.

--- tarmii/theme/browser/viewlets.py
+++ tarmii/theme/browser/viewlets.py
@@ -4,13 +4,11 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 class TARMIIGlobalSectionsViewlet(GlobalSectionsViewlet):
 
-    index = ViewPageTemplateFile('templates/sections.pt')
-
     def update(self):
         super(TARMIIGlobalSectionsViewlet, self).update()
         
         for x in range(len(self.portal_tabs)):
             self.portal_tabs[x]['name'] =\
-                self.context.translate(_('action_' + self.portal_tabs[x]['id']))
+                self.context.translate(_(self.portal_tabs[x]['id']))
                 # the translations labels: action_topictree, action_evaluations
                 # etc, are defined in actions.xml
