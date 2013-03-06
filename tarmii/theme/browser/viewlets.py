d68ff38d0b66e373bea5af555d0982273d17f760
witek <witekdev@gmail.com>
2013-03-06 09:12:30 +0200

    manually translate portal tabs in TARMIIGlobalSectionsViewlet.

--- tarmii/theme/browser/viewlets.py
+++ tarmii/theme/browser/viewlets.py
@@ -5,3 +5,13 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 class TARMIIGlobalSectionsViewlet(GlobalSectionsViewlet):
 
     index = ViewPageTemplateFile('templates/sections.pt')
+
+    def update(self):
+        super(TARMIIGlobalSectionsViewlet, self).update()
+
+        self.portal_tabs[0]['name'] = self.context.translate(_(u'Actvities'))
+        self.portal_tabs[1]['name'] = self.context.translate(_(u'Resources'))
+        self.portal_tabs[2]['name'] = self.context.translate(_(u'Videos'))
+        self.portal_tabs[3]['name'] = self.context.translate(_(u'Classlists'))
+        self.portal_tabs[4]['name'] = self.context.translate(_(u'Assessments'))
+        self.portal_tabs[5]['name'] = self.context.translate(_(u'Evaluations'))
