a701bfe1ba9db2851b10f2e77812cacb90e58af0
witek <witekdev@gmail.com>
2013-03-08 13:09:34 +0200

    convert home page draft to a browser view, remove home.html from theming mechanism. Enable home portal tab and link to @@home, add new translations.

---
+++ tarmii/theme/browser/home.py
@@ -0,0 +1,26 @@
+from five import grok
+from zope.interface import Interface
+from Products.CMFCore.utils import getToolByName
+
+from tarmii.theme import MessageFactory as _
+
+grok.templatedir('templates')
+
+class HomeView(grok.View):
+    """ View to display the front page
+    """
+    grok.context(Interface)
+    grok.name('home')
+    grok.template('home')
+    grok.require('zope2.View')
+
+    def portal_root(self):
+        """ URL for the portal
+        """
+        return self.context.absolute_url()
+
+    def members_folder(self):
+        """ URL for the members folder
+        """
+        pm = getToolByName(self.context, 'portal_membership')        
+        return pm.getHomeUrl()
