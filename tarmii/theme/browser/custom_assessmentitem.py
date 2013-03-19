280d02ae4996b3743b2bd43829fd9d0308222513
witek <witekdev@gmail.com>
2013-03-19 14:10:44 +0200

    prevent activities from being deleted or edited if they are in use by an existing assessment.

---
+++ tarmii/theme/browser/custom_assessmentitem.py
@@ -0,0 +1,54 @@
+from five import grok
+from plone.directives import dexterity
+
+from zope.app.intid.interfaces import IIntIds
+from zope.component import getUtility
+from zope.component.hooks import getSite
+from zc.relation.interfaces import ICatalog
+from plone.directives import dexterity
+from plone.uuid.interfaces import IUUID
+
+#from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
+from Products.statusmessages.interfaces import IStatusMessage
+
+from upfront.assessmentitem.content.assessmentitem import IAssessmentItem
+from tarmii.theme.interfaces import ITARMIIThemeLayer
+from tarmii.theme import MessageFactory as _
+
+grok.templatedir('templates')
+
+class View(dexterity.DisplayForm):
+    grok.layer(ITARMIIThemeLayer)
+    grok.context(IAssessmentItem)
+    grok.require('zope2.View')
+    grok.template('custom-assessmentitem-view')
+
+    def assessmentitem(self):
+        """ Return the currently selected assessmentitem id
+        """
+        return self.context.id
+
+    def creationdate(self):
+        return self.context.created().strftime('%d %B %Y')
+
+    def review_state(self):
+        wftool = getToolByName(self.context, 'portal_workflow')
+        return wftool.getInfoFor(self.context, 'review_state')
+
+    def used_in_assessments(self):
+        """ Check if activity is used in any assessments.
+        """
+        catalog = getUtility(ICatalog)
+        intids = getUtility(IIntIds)
+        result = catalog.findRelations({
+            'to_id': intids.getId(self.context),
+            'from_attribute': 'assessment_items'
+            })
+        try:
+            rel = result.next()
+            activity_unwrapped = rel.from_object
+        except StopIteration:
+            return False
+
+        return True
