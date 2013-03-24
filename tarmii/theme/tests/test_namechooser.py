9648f25d957accbfdd2c5e2236b74f9f425dc101
witek <witekdev@gmail.com>
2013-03-24 22:12:45 +0200

    fix unit tests for namechooser utility. tarmii.theme unit tests now working again.

--- tarmii/theme/tests/test_namechooser.py
+++ tarmii/theme/tests/test_namechooser.py
@@ -1,7 +1,9 @@
+from zope.component import getUtility
 from zope.container.interfaces import INameChooser
 
 from plone.dexterity.utils import createContent
 from plone.dexterity.utils import createContentInContainer
+from plone.i18n.normalizer.interfaces import IURLNormalizer
 
 from tarmii.theme.tests.base import TarmiiThemeTestBase
 from tarmii.theme.namechooser import AssessmentItemNameChooser
@@ -16,19 +18,21 @@ class TestNameChooser(TarmiiThemeTestBase):
 
     def test_checkName(self):
         namechooser = INameChooser(self.activities)
+        normalizer = getUtility(IURLNormalizer)
         obj = createContent(
             'upfront.assessmentitem.content.assessmentitem')
-        self.assertEqual(namechooser.checkName('Q001', obj), True)
+        test_name = normalizer.normalize('Q001')
+        self.assertEqual(namechooser.checkName(test_name, obj), True)
         obj = createContentInContainer(self.activities,
             'upfront.assessmentitem.content.assessmentitem')
-        self.assertEqual(namechooser.checkName('Q001', obj), False)
+        self.assertEqual(namechooser.checkName(test_name, obj), False)
 
     def test_chooseName(self):
         namechooser = INameChooser(self.activities)
         obj = createContentInContainer(self.activities,
-            'upfront.assessmentitem.content.assessmentitem')
-        self.assertEqual(namechooser.chooseName(None, obj), 'Q001')
+            'upfront.assessmentitem.content.assessmentitem')        
+        self.assertEqual(namechooser.chooseName(None, obj), 'q001')
         obj = createContent(
             'upfront.assessmentitem.content.assessmentitem')
-        self.assertEqual(namechooser.chooseName(None, obj), 'Q002')
+        self.assertEqual(namechooser.chooseName(None, obj), 'q002')
         self.assertEqual(namechooser.chooseName('custom', obj), 'custom')
