c1643cfcae57a2f0192274a6c3aefd3f54dc27ab
witek <witekdev@gmail.com>
2013-03-26 20:42:01 +0200

    evaluerings -> evaluasies in afrikaans translation. fix delete videos cancel button overlay issue.

--- tarmii/theme/static/js/overlays.js
+++ tarmii/theme/static/js/overlays.js
@@ -37,6 +37,15 @@ $(function($) {
         closeselector: '[name="form.button.Cancel"]',
         });
 
+    $('a.delete_confirmation').prepOverlay({
+        subtype: 'ajax',
+        filter: '#content>*',
+        formselector: '#form',
+        noform: 'reload',
+        redirect: url,
+        closeselector: '[name="form.button.Cancel"]',
+        });
+
 });
 
 // disable multi submit on add forms (save button)
