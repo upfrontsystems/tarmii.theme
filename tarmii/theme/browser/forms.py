59581dcbe7e92cc9968e36c11994bce1a01af8a8
witek <witekdev@gmail.com>
2013-03-27 09:57:37 +0200

    ActivityAddForm - work in progress.

--- tarmii/theme/browser/forms.py
+++ tarmii/theme/browser/forms.py
@@ -1,3 +1,4 @@
+from sets import Set
 from five import grok
 from zope.component.hooks import getSite
 from plone.directives import dexterity
@@ -26,3 +27,24 @@ class TeacherResourceEditForm(dexterity.EditForm):
         # both save and cancel buttons redirect to the same place
         site = getSite()
         return '%s/resources' % (site.absolute_url())
+
+
+class ActivityAddForm(dexterity.AddForm):
+    grok.name('upfront.assessmentitem.content.assessmentitem')
+    grok.layer(ITARMIIThemeLayer)
+
+    def updateWidgets(self):
+        """ Customize widget options before rendering the form. """
+        super(ActivityAddForm, self).updateWidgets()
+
+        if 'IRating.rating_scale' in Set(self.widgets.keys()):
+            self.widgets['IRating.rating_scale'].field.default =\
+                [{'rating': 4,
+                  'label': self.context.translate(_(u'Excellent'))},
+                 {'rating': 3,
+                  'label': self.context.translate(_(u'Good'))},
+                 {'rating': 2,
+                  'label': self.context.translate(_(u'Satisfactory'))},
+                 {'rating': 1,
+                  'label': self.context.translate(_(u'Needs improvement'))}]
+
