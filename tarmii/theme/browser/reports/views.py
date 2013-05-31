efffbdab0b224a9b07dca9f01f6e152340f4f7f9
witek <witekdev@gmail.com>
2013-05-31 09:53:35 +0200

    class performance chart convert assessment filter to use evaluationsheets in the drop down instead of assessments.

--- tarmii/theme/browser/reports/views.py
+++ tarmii/theme/browser/reports/views.py
@@ -109,68 +109,40 @@ class ClassPerformanceForActivityChartView(grok.View):
     def data(self):
         # if we are here, evaluations exist
 
-        classlist_uid = self.request.get('classlist', '')
-        assessment_uid = self.request.get('assessment', '')
+        evaluationsheet_uid = self.request.get('evaluationsheet', '')
         activity_uid = self.request.get('activity', '')
-
-        assessment = uuidToObject(assessment_uid)
-        # find all evaluationsheets that reference the chosen assessment
-        ref_catalog = getUtility(ICatalog)
-        intids = getUtility(IIntIds)
-        result = ref_catalog.findRelations({'to_id': intids.getId(assessment)})
-        evalsheet_rel_list = []
-        notfinished = True;
-        while notfinished:
-            try:
-                rel = result.next()
-                if rel.from_object.portal_type == \
-                      'upfront.assessment.content.evaluationsheet':
-                    # only track references from evaluationsheet objects.
-                    evalsheet_rel_list.append(rel.from_object)
-            except StopIteration:
-                notfinished = False;
-
-        # make sure that we use evaluationsheets only for the selected classlist
-        filtered_evalsheet_rel_list = []
-        for evalsheet in evalsheet_rel_list:
-            if evalsheet.classlist.to_object == uuidToObject(classlist_uid):
-                filtered_evalsheet_rel_list.append(evalsheet)
 
         # iterate through all evaluations that reference the current assessment
         activity = uuidToObject(activity_uid)
-        pm = getSite().portal_membership
-        members_folder = pm.getHomeFolder()
 
         rating_scale = {}
         count_dict = {}
         lookup_dict = {}
-        for evalsheet in members_folder.evaluations.getFolderContents():
-            if evalsheet.getObject() in filtered_evalsheet_rel_list:
-                for eval_obj in evalsheet.getObject().getFolderContents():
-                    # 1 eval_obj/learner
-                    for x in range(len(eval_obj.getObject().evaluation)):
-                        uid = eval_obj.getObject().evaluation[x]['uid']
-                        if activity_uid == uid:
-                            # found matching activity data
-                            eval_obj.getObject().evaluation[x]
-                            # get the rating_scale, init the count and lookup 
-                            # dictionaries only once.
-                            if rating_scale == {}:
-                                rating_scale = eval_obj.getObject() \
-                                               .evaluation[x]['rating_scale']
+        for eval_obj in uuidToObject(evaluationsheet_uid).getFolderContents():
+            # 1 eval_obj/learner
+            for x in range(len(eval_obj.getObject().evaluation)):
+                uid = eval_obj.getObject().evaluation[x]['uid']
+                if activity_uid == uid:
+                    # found matching activity data
+                    eval_obj.getObject().evaluation[x]
+                    # get the rating_scale, init the count and lookup 
+                    # dictionaries only once.
+                    if rating_scale == {}:
+                        rating_scale = eval_obj.getObject() \
+                                       .evaluation[x]['rating_scale']
 
-                                for z in range(len(rating_scale)):
-                                    # init the score counter and the lookup dict
-                                    count_dict[rating_scale[z]['label']] = 0
-                                    lookup_dict[rating_scale[z]['rating']] = \
-                                        rating_scale[z]['label']
+                        for z in range(len(rating_scale)):
+                            # init the score counter and the lookup dict
+                            count_dict[rating_scale[z]['label']] = 0
+                            lookup_dict[rating_scale[z]['rating']] = \
+                                rating_scale[z]['label']
 
-                            # use lookup dict to interpret ratings
-                            rating=eval_obj.getObject().evaluation[x]['rating']
-                            # do not add unrated (0) entries and 
-                            # explicitly unrated (-1) entries into the count
-                            if rating != 0 and rating != -1:
-                               count_dict[lookup_dict[rating]] += 1
+                    # use lookup dict to interpret ratings
+                    rating=eval_obj.getObject().evaluation[x]['rating']
+                    # do not add unrated (0) entries and 
+                    # explicitly unrated (-1) entries into the count
+                    if rating != 0 and rating != -1:
+                       count_dict[lookup_dict[rating]] += 1
 
         # parse count_dict into a format that the charting code accepts
         value_data = ()
@@ -185,9 +157,9 @@ class ClassPerformanceForActivityChartView(grok.View):
             value_data = value_data + (value,)
             # append 'learner' or 'learners' to the number of learners.
             if value > 1:
-                learner_str = ' learners'
+                learner_str = self.context.translate(_(u' learners'))
             else:
-                learner_str = ' learner'
+                learner_str = self.context.translate(_(u' learner'))
             category_labels.append(str(value) + learner_str)
 
         # make sure we are not supplying of value_data of all zeros 
@@ -250,7 +222,8 @@ class ClassPerformanceForActivityView(grok.View, ReportViewsCommon):
             options (if applicable)
         """
         self.classlist_uid = self.request.get('classlist_uid_selected', '')
-        self.assessment_uid = self.request.get('assessment_uid_selected', '')
+        self.evaluationsheet_uid = \
+            self.request.get('evaluationsheet_uid_selected', '')
         self.activity_uid = self.request.get('activity_uid_selected', '')
 
         if self.classlist_uid == '':
@@ -268,19 +241,17 @@ class ClassPerformanceForActivityView(grok.View, ReportViewsCommon):
                             .getObject()
                 self.classlist_uid = IUUID(classlist)
 
-        if self.assessment_uid == '':
-            pm = getSite().portal_membership
-            members_folder = pm.getHomeFolder()
-            if members_folder == None:
-                return []
-            if len(members_folder.assessments.getFolderContents()) == 0:
-                # assessments contains no activities
-                self.assessment_id = ''
+        if self.evaluationsheet_uid == '':
+            evaluationsheets = self.evaluationsheets()
+            if len(self.evaluationsheets()) == 0:
+                # classlist contains no evaluationsheets
+                self.evaluationsheet_uid = ''
                 return []
             else:                
-                # no assessment selected so pick first one in list
-                assessment = members_folder.assessments.getFolderContents()[0] \
-                             .getObject()
+                # no evaluationsheet selected so pick first one in list
+                ev = evaluationsheets[0]
+                self.evaluationsheet_uid = IUUID(ev)  
+                assessment = ev.assessment.to_object
                 self.assessment_uid = IUUID(assessment)
                 if len(assessment.assessment_items) != 0:
                     self.activity_uid = \
@@ -288,7 +259,8 @@ class ClassPerformanceForActivityView(grok.View, ReportViewsCommon):
         elif self.activity_uid == '':
             # this executes when one is picking an assessment with activities
             # after having picked an assessment with no activities
-            assessment = uuidToObject(self.assessment_uid)
+            ev = uuidToObject(self.evaluationsheet_uid)
+            assessment = ev.assessment.to_object
             if len(assessment.assessment_items) != 0: 
                 # pick the first activity from the assessment's activities
                 self.activity_uid = \
@@ -298,7 +270,8 @@ class ClassPerformanceForActivityView(grok.View, ReportViewsCommon):
             # this scenario can happen when switching assessments from an 
             # eg. assessment1 that contains activity3 but the assessment2 does
             # not
-            assessment = uuidToObject(self.assessment_uid)
+            ev = uuidToObject(self.evaluationsheet_uid)
+            assessment = ev.assessment.to_object
             act = uuidToObject(self.activity_uid)
             # if activity selected is not in the newly selected assessment
             if not act in [x.to_object for x in assessment.assessment_items]:                
@@ -307,29 +280,40 @@ class ClassPerformanceForActivityView(grok.View, ReportViewsCommon):
                     self.activity_uid = \
                         IUUID(assessment.assessment_items[0].to_object)
 
-    def assessments(self):
-        """ return all of the assessments of the current user
+    def evaluationsheets(self):
+        """ return all of the evaluationsheet of the current classlist
         """
         pm = getSite().portal_membership
         members_folder = pm.getHomeFolder()
         if members_folder == None:
             return []
-        return members_folder.assessments.getFolderContents()
+
+        contentFilter = \
+            {'portal_type': 'upfront.assessment.content.evaluationsheet'}
+        evaluationsheets = \
+            members_folder.evaluations.getFolderContents(contentFilter,
+                                                         full_objects=True)
+
+        # make sure that we use evaluationsheets only for the selected classlist
+        filtered_evalsheet_list = []
+        for evalsheet in evaluationsheets:
+            if evalsheet.classlist.to_object == uuidToObject(self.classlist_uid):
+                filtered_evalsheet_list.append(evalsheet)
+        
+        return filtered_evalsheet_list
 
     def activities(self):
         """ return all of the activities of a specific assessment
         """
-        if self.assessment_uid == '':
+        if self.evaluationsheet_uid == '':
             return []
         else:
-            assessment = uuidToObject(self.assessment_uid)
+            ev = uuidToObject(self.evaluationsheet_uid)
+            assessment = ev.assessment.to_object
         return assessment.assessment_items
 
-    def selected_classlist(self):
-        return self.classlist_uid
-
-    def selected_assessment(self):
-        return self.assessment_uid
+    def selected_evaluationsheet(self):
+        return self.evaluationsheet_uid
 
     def selected_activity(self):
         return self.activity_uid
@@ -342,43 +326,18 @@ class ClassPerformanceForActivityView(grok.View, ReportViewsCommon):
             assessment, at least one of these evaluationsheet must contain 
             evaluations
         """
-        if self.assessment_uid == '':
+        if self.evaluationsheet_uid == '':
             return []
         if self.activity_uid == '':
             return []
+            
+        # make sure that there are at least some evaluation objects in the
+        # selected evaluationsheet, ie. the classlists used for generating 
+        # evaluations were not empty.
+        if len(uuidToObject(self.evaluationsheet_uid).getFolderContents()) != 0:
+            return uuidToObject(self.evaluationsheet_uid).getFolderContents()
 
-        assessment = uuidToObject(self.assessment_uid)
-        pm = getSite().portal_membership
-        members_folder = pm.getHomeFolder()
-        if len(members_folder.evaluations.getFolderContents()) == 0:
-            # no evaluations
-            return []
-        else:
-            # find all evaluationsheets that reference the chosen assessment
-            ref_catalog = getUtility(ICatalog)
-            intids = getUtility(IIntIds)
-            result = \
-                ref_catalog.findRelations({'to_id': intids.getId(assessment)})
-            rel_list = []
-            notfinished = True;
-            while notfinished:
-                try:
-                    rel = result.next()
-                    if rel.from_object.portal_type == \
-                          'upfront.assessment.content.evaluationsheet':
-                        # only track references from evaluation objects.
-                        rel_list.append(IUUID(rel.from_object))
-                except StopIteration:
-                    notfinished = False;
-             
-            # make sure that there are at least some evaluation objects in the
-            # evaluationsheets, ie. the classlists used for generating 
-            # evaluations were not empty.
-            for evaluationsheet in rel_list:
-                if len(uuidToObject(evaluationsheet).getFolderContents()) != 0:
-                    return rel_list
-
-            return []
+        return []
 
 
 class ClassProgressChartView(grok.View):
