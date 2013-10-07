b8956e0187769601dc924c9c7238e84899575d9d
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-10-07 15:39:58 +0200

    Return files to the client for testing purposes

--- tarmii/theme/tests/assessmentitems_xml_server.py
+++ tarmii/theme/tests/assessmentitems_xml_server.py
@@ -1,23 +1,70 @@
 import os
 import re
 import sys
+from DateTime import DateTime
 import BaseHTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 
 class IdsRequestHandler(SimpleHTTPRequestHandler):
     
-    print 'Loading xml ...'
-    filename = os.path.join(os.getcwd(),
-                            'src', 'tarmii.theme', 'tarmii', 'theme',
-                            'tests', 'assessmentitem_ids.xml')
-    xml_file = open(filename,'rb')
-    content = xml_file.read()
-    xml_file.close()
-    print 'done.'
+    components = ['src', 'tarmii.theme', 'tarmii', 'theme', 'tests']
+    base_path = os.path.join(os.getcwd(), *components)
 
-    def do_GET(self):
-        self.wfile.write(self.content)
+    def send_head(self):
+        path = self.translate_path(self.path)                                   
+        f = None                                                                
+        if os.path.isdir(path):                                                 
+            if not self.path.endswith('/'):                                     
+                # redirect browser - doing basically what apache does           
+                self.send_response(301)                                         
+                self.send_header("Location", self.path + "/")                   
+                self.end_headers()                                              
+                return None                                                     
+            for index in "index.html", "index.htm":                             
+                index = os.path.join(path, index)                               
+                if os.path.exists(index):                                       
+                    path = index                                                
+                    break                                                       
+                else:                                                               
+                    return self.list_directory(path)                                
+
+        ctype = self.guess_type(path)                                           
+        cdisposition = None
+        if self.path.endswith('/@@assessmentitem-ids-xml'):
+            path = os.path.join(self.base_path, 'assessmentitem_ids.xml')
+            ctype = 'text/xml'
+        elif self.path.endswith('/@@assessmentitem-xml'):
+            path = os.path.join(self.base_path, 'assessmentitems.zip')
+            cdisposition = "attachment; filename=assessmentitems.zip"
+            ctype = 'application/octet-stream'
+
+        try:                                                                    
+            # Always read in binary mode. Opening files in text mode may cause  
+            # newline translations, making the actual size of the content       
+            # transmitted *less* than the content-length!                       
+            f = open(path, 'rb')                                                
+        except IOError:                                                         
+            self.send_error(404, "File not found")                              
+            return None                                                         
+
+        self.send_response(200)                                                 
+        self.send_header("Content-type", ctype)                                 
+        if cdisposition:
+            self.send_header("Content-Disposition", cdisposition)
+        fs = os.fstat(f.fileno())                                               
+        self.send_header("Content-Length", str(fs[6]))                          
+        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))   
+        self.send_header('expires', 0)
+        self.end_headers()                                                      
+        return f 
+
+    def do_POST(self):
+        """Serve a POST request."""                                              
+        f = self.send_head()                                                    
+        if f:                                                                   
+            self.copyfile(f, self.wfile)                                        
+            f.close() 
 
 
 HandlerClass = IdsRequestHandler
