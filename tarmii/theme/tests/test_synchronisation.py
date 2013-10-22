fb79b1a06f28c1edef8499ca03364f97aa978144
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-10-22 10:49:31 +0200

    Test the returned zip file and content

--- tarmii/theme/tests/test_synchronisation.py
+++ tarmii/theme/tests/test_synchronisation.py
@@ -1,5 +1,7 @@
 import lxml
 import requests
+from zipfile import ZipFile
+from cStringIO import StringIO
 
 from zope.component import getUtility
 
@@ -14,10 +16,24 @@ class TestSynchronisation(TarmiiThemeTestBase):
         view = self.portal.restrictedTraverse('@@assessmentitem-ids-xml')
         result = view()
         tree = lxml.etree.fromstring(result)
-        self.assertEqual(len(tree.findall('assessmentitem')), 4)
+        num_activities = len(self.portal.activities.objectIds())
+        items = tree.findall('assessmentitem')
+        self.assertEqual(len(items), num_activities)
+        for number in range(1, num_activities + 1):
+            item = tree.find('assessmentitem[@id="assessmentitem%s"]' % number)
+            assert item is not None
 
-    def test_fetch_assessments(self):
-        self.fail()
+    def test_fetch_assessmentitems(self):   
+        view = self.portal.restrictedTraverse('@@assessmentitem-ids-xml')
+        xml = view()
+
+        view = self.portal.restrictedTraverse('@@assessmentitem-xml')
+        view.request.form['xml'] = xml
+        result = view()
+        zipio = StringIO(result)
+        zipfile = ZipFile(zipio, 'r')
+        assert ('assessmentitems.xml' in zipfile.namelist(),
+                'Could no find assessments.xml')
     
     def test_marshal_item(self):  
         view = self.portal.restrictedTraverse('@@assessmentitem-xml')
